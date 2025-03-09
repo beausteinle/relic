125f93dbd0651456deb2dc73e8504af9940e3589
Beau Steinle <beausteinle@live.com>
2025-03-08 22:34:42 -0700

    add: repo linter with eslint + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { Button } from './components/button/Button'
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button
+          onClick={() => window.alert('Clicked!')}
+          size="large"
+          text="Click Me!"
+        />
       </header>
     </div>
-  );
+  )
 }
-
-export default App;
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,31 +1,30 @@
-import React from "react";
 import './button.css'
 
-declare const ButtonSizes: ['small', 'medium', 'large'];
-type ButtonSize = typeof ButtonSizes[number];
-
 interface ButtonProps {
-  onClick: () => void;
-  text: string;
-  className?: string;
-  size?: ButtonSize;
-  disabled?: boolean;
+  onClick: () => void
+  text: string
+  className?: string
+  size?: 'small' | 'medium' | 'large'
+  disabled?: boolean
 }
 
 export const Button = ({
-  onClick, 
-  text, 
-  className, 
-  size, 
-  disabled
+  onClick,
+  text,
+  className,
+  size,
+  disabled,
 }: ButtonProps) => {
   return (
-    <button 
-      onClick={onClick} 
-      className={className || `color-white bg-color-primary border-none border-radius-5 size-${size ?? 'medium'}`}
+    <button
+      onClick={onClick}
+      className={
+        className ||
+        `color-white bg-color-primary border-none border-radius-5 size-${size ?? 'medium'}`
+      }
       disabled={disabled}
     >
       {text}
     </button>
-  );
+  )
 }
