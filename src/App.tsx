82baa1c39ec38d127e70dad12d68ebc6c892482d
Beau Steinle <beausteinle@live.com>
2025-03-10 23:35:31 -0600

    Skeleton for input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import logo from './logo.svg'
 import './App.css'
-import { Button } from './components/button/Button'
+import { Button } from './components/alpha/button/Button'
+import { Input } from './components/alpha/input/input'
 
 export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Input />
         <Button
           onClick={() => window.alert('Clicked!')}
           size="large"
