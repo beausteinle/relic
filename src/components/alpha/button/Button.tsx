84359e1c8aa6a1eefe7763216b8a3feb2c909aac
beausteinle <beausteinle@live.com>
2025-03-12 20:44:02 -0600

    migrate to vite (#5)

--- src/components/alpha/button/Button.tsx
+++ src/components/alpha/button/Button.tsx
@@ -1,13 +1,14 @@
-import './button.css'
+import "./button.css";
 
 export type ButtonProps = {
-  onClick: () => void
-  text: string
-  className?: string
-  size?: 'small' | 'medium' | 'large'
-  disabled?: boolean
-  style?: React.CSSProperties
-}
+  onClick: () => void;
+  text?: string;
+  className?: string;
+  size?: "small" | "medium" | "large";
+  disabled?: boolean;
+  style?: React.CSSProperties;
+  children?: React.ReactNode;
+};
 
 export const Button = ({
   onClick,
@@ -16,18 +17,21 @@ export const Button = ({
   size,
   disabled,
   style,
+  children,
 }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
       className={
         className ||
-        `color-white bg-color-primary border-none border-radius-5 size-${size ?? 'medium'}`
+        `color-white bg-color-primary border-none border-radius-5 size-${
+          size ?? "medium"
+        }`
       }
       disabled={disabled}
       style={style}
     >
-      {text}
+      {text || children}
     </button>
-  )
-}
+  );
+};
