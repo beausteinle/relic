27396b96d94a5241822b6dfe42b38d43979e9e12
beausteinle <beausteinle@live.com>
2025-03-14 00:23:54 -0600

    Add first pipeline action (#9)
    * Resolve issues blocking npm publish
    
    * add linter action
    
    * fix lint settings
    
    * fix filename
    
    * test error
    
    * remove continueOnError
    
    * fix lint error
    
    * create publish action on release only
    
    * update the relase flow to be on push
    
    * rename token

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
-import js from "@eslint/js" // Base configs & environments
-import { FlatCompat } from "@eslint/eslintrc"
+import js from '@eslint/js' // Base configs & environments
+import { FlatCompat } from '@eslint/eslintrc'
 
-import tsParser from "@typescript-eslint/parser"
-import tsPlugin from "@typescript-eslint/eslint-plugin"
-import reactPlugin from "eslint-plugin-react"
-import reactHooksPlugin from "eslint-plugin-react-hooks"
-import importPlugin from "eslint-plugin-import"
-import jsxA11yPlugin from "eslint-plugin-jsx-a11y"
-import prettierPlugin from "eslint-plugin-prettier"
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import importPlugin from 'eslint-plugin-import'
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 // Create a compat instance to translate old-style "extends"
 const compat = new FlatCompat({
@@ -19,21 +19,21 @@ const compat = new FlatCompat({
 export default [
   // Files/folders to ignore
   {
-    ignores: ["node_modules", "dist", "build", "eslint.config.js"],
+    ignores: ['node_modules', 'dist', 'build', 'eslint.config.js'],
   },
 
   // Extend from a set of recommended configurations
   ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:prettier/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'prettier'
   ),
 
   // Custom configuration
@@ -41,42 +41,42 @@ export default [
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+        ecmaVersion: 'latest',
+        sourceType: 'module',
         ecmaFeatures: { jsx: true },
       },
       // Manually list common browser + node globals
       globals: {
-        window: "readonly",
-        document: "readonly",
-        navigator: "readonly",
-        console: "readonly",
-        process: "readonly",
-        __dirname: "readonly",
-        module: "readonly",
-        require: "readonly",
+        window: 'readonly',
+        document: 'readonly',
+        navigator: 'readonly',
+        console: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
       },
     },
     plugins: {
-      "@typescript-eslint": tsPlugin,
+      '@typescript-eslint': tsPlugin,
       react: reactPlugin,
-      "react-hooks": reactHooksPlugin,
+      'react-hooks': reactHooksPlugin,
       import: importPlugin,
-      "jsx-a11y": jsxA11yPlugin,
+      'jsx-a11y': jsxA11yPlugin,
       prettier: prettierPlugin,
     },
     rules: {
-      "prettier/prettier": ["error"],
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "warn",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
+      'prettier/prettier': ['error'],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
       ],
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
