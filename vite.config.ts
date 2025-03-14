2058f271e6f2498605ffcf6aa7f1f500915b4fcf
Beau Steinle <beausteinle@live.com>
2025-03-13 22:16:37 -0600

    More vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import dts from 'vite-plugin-dts'
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import { glob } from 'glob'
+import { fileURLToPath } from 'url'
 
 export default defineConfig({
   plugins: [
@@ -11,6 +13,8 @@ export default defineConfig({
     libInjectCss(),
     dts({
       entryRoot: path.resolve(__dirname, 'src'),
+      include: ['src'],
+      tsconfigPath: path.resolve(__dirname, 'tsconfig.json'),
     }),
   ],
   build: {
@@ -18,16 +22,40 @@ export default defineConfig({
       entry: path.resolve(__dirname, 'src/index.tsx'),
       name: 'react-relic',
       fileName: (format) => `react-relic.${format}.js`,
+      formats: ['es'],
     },
     rollupOptions: {
       // Don't bundle React and ReactDOM, they are peer dependencies
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
         },
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
       },
+      input: Object.fromEntries(
+        glob
+          .sync('src/**/*.{ts,tsx}', {
+            ignore: [
+              'src/**/*.d.ts',
+              'src/App.*',
+              'src/main.tsx',
+              'index.css',
+              'src/assets/**/*.svg',
+            ],
+          })
+          .map((file) => {
+            return [
+              path.relative(
+                'src',
+                file.slice(0, file.length - path.extname(file).length)
+              ),
+              fileURLToPath(new URL(file, import.meta.url)),
+            ]
+          })
+      ),
     },
   },
 })
