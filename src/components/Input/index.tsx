b90bc6dc7c28f8cb49ed48a611443f644fb329d3
beausteinle <beausteinle@live.com>
2025-03-23 17:26:48 -0600

    Add input style

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,15 +4,38 @@ import styles from './styles.module.css'
 export type InputProps = {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
   value?: string
+  placeholder?: string
+  size?: 'small' | 'medium' | 'large'
+  className?: string
   style?: React.CSSProperties
   disabled?: boolean
 }
 
-const Input = ({ onChange, value, style, disabled }: InputProps) => {
+const getSize = (size: string | undefined) => {
+  switch (size) {
+    case 'small':
+      return styles.input_sm
+    case 'large':
+      return styles.input_lg
+    default:
+      return styles.input_md
+  }
+}
+
+const Input = ({
+  onChange,
+  value,
+  className,
+  style,
+  placeholder,
+  size,
+  disabled,
+}: InputProps) => {
   return (
     <input
-      className={styles.r_input}
+      className={`${styles.r_input} ${getSize(size)} ${className || ''}`}
       value={value}
+      placeholder={placeholder}
       onChange={onChange}
       style={style || { maxWidth: '400px' }}
       disabled={disabled}
